e73cdd95283ae05939989c8884019dcfa57900f8
Moritz E. Beber <midnighter@posteo.net>
2016-10-31 02:48:32 +0100

    add Rspec and factory girl for testing and fixtures

--- lib/multilingual/engine.rb
+++ lib/multilingual/engine.rb
@@ -1,6 +1,11 @@
 module Multilingual
   class Engine < ::Rails::Engine
     isolate_namespace Multilingual
-    config.generators.api_only = true
+
+    config.generators do |g|
+      g.api_only = true
+      g.test_framework :rspec, fixture: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
