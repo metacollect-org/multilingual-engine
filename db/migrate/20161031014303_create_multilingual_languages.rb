51ec35842f89139e8f110c2b602575cc0a5c23d7
Moritz E. Beber <midnighter@posteo.net>
2016-11-16 23:08:47 +0100

    require non-null values on database level

--- db/migrate/20161031014303_create_multilingual_languages.rb
+++ db/migrate/20161031014303_create_multilingual_languages.rb
@@ -1,8 +1,7 @@
 class CreateMultilingualLanguages < ActiveRecord::Migration[5.0]
   def change
     create_table :multilingual_languages do |t|
-      t.string :code, limit: 2  # ISO 639-1
+      t.string :code, limit: 2, unique: true, null: false  # ISO 639-1
     end
-    add_index :multilingual_languages, :code, unique: true
   end
 end
--- db/migrate/20161031070524_create_multilingual_translations.rb
+++ db/migrate/20161031070524_create_multilingual_translations.rb
@@ -1,9 +1,9 @@
 class CreateMultilingualTranslations < ActiveRecord::Migration[5.0]
   def change
     create_table :multilingual_translations do |t|
-      t.string :content
-      t.integer :language_id
-      t.references :translatable, polymorphic: true, index: { name: 'index_multilingual_translations_translatable' }
+      t.string :content, null: false
+      t.integer :language_id, null: false
+      t.references :translatable, null: false, polymorphic: true, index: { name: 'index_multilingual_translations_translatable' }
     end
     add_foreign_key :multilingual_translations, :multilingual_languages, column: :language_id
   end
